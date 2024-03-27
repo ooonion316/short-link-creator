cdc2ee8763b7dac33d89cd7376b9f16dbccfaf2b
Zhang Yunxiang <316yxz@gmail.com>
2024-03-27 15:42:59 +0800

    refactor: 优化用户模块 Redis 前缀

--- user/src/main/java/pers/zyx/shortlink/constant/UserRedisCacheConstant.java
+++ user/src/main/java/pers/zyx/shortlink/constant/UserRedisCacheConstant.java
@@ -4,5 +4,13 @@ package pers.zyx.shortlink.constant;
  * 用户模块 Redis 缓存常量
  */
 public class UserRedisCacheConstant {
+    /**
+     * 用户注册锁前缀
+     */
     public static final String USER_REGISTER_LOCK = "short-link:lock_user_register:";
+
+    /**
+     * 用户登录前缀
+     */
+    public static final String USER_LOGIN_PREFIX = "short-link:logged_in:";
 }
