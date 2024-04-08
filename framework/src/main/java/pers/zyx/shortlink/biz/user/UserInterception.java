c042783aed2f4b56bb23f0f1bc609cf83274373c
Zhang Yunxiang <316yxz@gmail.com>
2024-04-09 00:43:31 +0800

    refactor: 为用户拦截器添加执行顺序

--- framework/src/main/java/pers/zyx/shortlink/biz/user/UserInterception.java
+++ framework/src/main/java/pers/zyx/shortlink/biz/user/UserInterception.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson2.JSON;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +19,7 @@ import java.util.Objects;
 /**
  * 用户拦截器
  */
+@Order(0)
 @RequiredArgsConstructor
 public class UserInterception implements HandlerInterceptor {
     private final StringRedisTemplate stringRedisTemplate;
