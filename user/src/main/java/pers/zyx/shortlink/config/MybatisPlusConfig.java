51b401ca5aa9707ce945852bcd4ff2b6dfaa7cba
Zhang Yunxiang <316yxz@gmail.com>
2024-04-12 11:03:37 +0800

    fix: 用户模块添加 Mybatis Plus 分页插件

---
+++ user/src/main/java/pers/zyx/shortlink/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package pers.zyx.shortlink.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Mybatis Plus 配置
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 添加分页插件
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
